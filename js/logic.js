8c85283701cb196017161f4f23eac9f3fcc0756b
Sharon Zhou <sharonzhou12@gmail.com>
2013-11-19 00:19:02 -0500

    saves to database but does not update to the next form(s)

--- js/logic.js
+++ js/logic.js
@@ -1,20 +1,40 @@
     var counter = 1;
     var completedForms = new Object();
 
-    $("#current-form" ).submit(function( event ) {
+        // on enter
+        $('form').keypress(function (e) {
+            if (e.which == 13) {
+
+        // ajax send topic and response to php
+        $.ajax({
+            type: "GET",
+            url: "survey.php",
+            data: "topic=" + $('#current-form .topic').attr("topic") + "\u0026response=" + $('#current-form .response').val(),
+            success: nextForm
+        })
+    }
+});
+
+    function nextForm() {
+
+        // check if true? doesn't work for the first one.. not necessary code
         completed = new Boolean;
 
         if ( $("#current-form input").val() !== "" ) {
+
             completed = true;
         }
-        else {
+        else {       
+            
             completed = false;
-        }
+
+            }
 
         completedForms[counter] = completed;
         console.log(completedForms[counter]);
 
-        counter ++;
+        // next forms
+        counter++;
 
         if (counter != 16 ) {
             event.preventDefault();
@@ -26,4 +46,5 @@
             $("#current-form *").replaceWith($("#finish-survey"));
         }
 
-    });
+    // close keypress on enter
+    }

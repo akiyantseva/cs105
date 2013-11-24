5e32be4a26cd4b8ad0da5341778df745b4a17b17
Anna Kiyantseva <annakiyantseva@college.harvard.edu>
2013-11-23 22:33:18 -0500

    Added consent form, option to view and erase your data, toggles for the social + time conditions, and some other crap.

--- js/logic.js
+++ js/logic.js
@@ -1,29 +1,74 @@
-    var counter = 1;
-    var completedForms = new Object();
+counter = 1;
+completedForms = new Object();
 
-    $("#current-form" ).submit(function( event ) {
-        completed = new Boolean;
+mobMode = 1;
+timeMode = 1;
 
-        if ( $("#current-form input").val() !== "" ) {
-            completed = true;
-        }
-        else {
-            completed = false;
-        }
+$(function() {
+if (mobMode !== 1) {
+    $(".mob-info").hide();
+}
+else {
+    $(".mob-info").show();
+}
 
-        completedForms[counter] = completed;
-        console.log(completedForms[counter]);
+if (timeMode !== 1) {
+    $(".time-info").hide();
+}
+else {
+    $(".time-info").show();
+}
 
-        counter ++;
+});
 
-        if (counter != 16 ) {
-            event.preventDefault();
-            $("#current-form *").replaceWith($("#"+counter));
-            $("#"+counter + " input").focus();            
-        }
-        else {
-            event.preventDefault();
-            $("#current-form *").replaceWith($("#finish-survey"));
-        }
+$("#current-form").submit(function( event ) {
+    completed = new Boolean;
 
-    });
+    if ( $("#current-form input").val() !== "" ) {
+        completed = true;
+    }
+    else {
+        completed = false;
+    }
+
+    completedForms[counter] = completed;
+    console.log(completedForms[counter]);
+
+    counter ++;
+
+    if (counter != 40 ) {
+        event.preventDefault();
+        $("#current-form *").replaceWith($("#"+counter));
+        $("#"+counter + " input").focus();            
+    }
+    else {
+        event.preventDefault();
+        $("#current-form *").replaceWith($("#finish-survey"));
+    }
+
+});
+
+
+
+
+$('.consent-box input').click(function(){
+    if ($("#consent-box").is (':checked'))
+    {
+        $("#start-button").fadeIn(300);
+    }
+    else {
+        $("#start-button").fadeOut(300);
+    }
+});
+
+$('#continue-debrief').click(function(){
+    $(".container").replaceWith($("#debrief"));
+
+    formdata = JSON.stringify(completedForms);
+    outputdata = "";
+
+    for(var formid in completedForms){
+        outputdata += formid + ': ' + completedForms[formid]+'; ';
+    }
+    $("#formdata").append(outputdata);
+});
